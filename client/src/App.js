610b05e91c7b4955a5e1bd31cedcbc6c1cfb720c
Avinash <avinashkumar_arvind@srmuniv.edu.in>
2019-12-16 23:55:01 +0530

    Impemented redux for sending alerts on errors

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,26 @@ import Landing from "./components/layout/Landing";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import { Provider } from "react-redux";
+import store from "./store";
+import Alert from "./components/layout/Alert";
 
 const App = () => (
-  <Router>
-    <Fragment className="App">
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
